d6694b56bbbb1fa82010430901c227b0eed417a2
DavidBatkay <david.batkay@emanuel.ro>
2023-10-16 08:24:28 +0300

    git

---
+++ boxing-and-unboxing/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace boxing_and_unboxing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 3;
+            object obj = i;
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
